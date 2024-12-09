4c32aa4bdc0e79598526ca9be814b4075f786080
Fernando-Marin <125309714+Fernando-Marin@users.noreply.github.com>
2024-12-09 01:26:14 -0600

    Add files via upload
    Creación del index de la pagina

---
+++ Proyecto_web/Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba de Conexión</title>
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <!-- Encabezado con botones -->
+    <header class="bg-primary text-white text-center py-5">
+        <h1 class="text-white">Bienvenido al Sistema de Tutorías</h1>
+        <div class="d-flex justify-content-center mt-3">
+            <a href="./views/register.php" class="btn btn-success mx-2">Regístrate</a>
+            <a href="./views/login.php" class="btn btn-primary mx-2">Inicia Sesión</a>
+        </div>
+    </header>
+
+    <!-- Contenido principal -->
+
+
+</body>
+
+</html>
